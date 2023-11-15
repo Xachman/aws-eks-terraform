a00e899b2f92d7593dd5d5b5fe9f306f1dcc85d6
xachman <zironside@hotmail.com>
2023-11-15 02:03:05 +0000

    add http and https sec groups for cluster

--- cluster.tf
+++ cluster.tf
@@ -114,23 +114,22 @@ resource "aws_eks_addon" "ebs-csi" {
   }
 }
 
-resource "aws_security_group" "allow_cluster_http_https" {
-  description = "Allow HTTP/HTTPS between nodes"
-  vpc_id      = module.vpc.data.output.vpc_id
+resource "aws_security_group_rule" "node_http" {
+  type              = "ingress"    
+  security_group_id = module.eks.node_security_group_id
+  description      = "HTTP for nodes"
+  from_port        = 80
+  to_port          = 80
+  protocol         = "tcp"
+  source_security_group_id = module.eks.node_security_group_id
+}
 
-  ingress {
-    description      = "HTTPS for cluster"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    security_groups = [module.eks.data.output.cluster_primary_security_group_id]
-  }
-
-  ingress {
-    description      = "HTTP for cluster"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    security_groups = [module.eks.data.output.cluster_primary_security_group_id]
-  }
+resource "aws_security_group_rule" "node_https" {
+  type              = "ingress"    
+  security_group_id = module.eks.node_security_group_id
+  description      = "HTTPS for nodes"
+  from_port        = 443
+  to_port          = 443
+  protocol         = "tcp"
+  source_security_group_id = module.eks.node_security_group_id
 }
