976b0166102dc0f89e06de5cd7785fe88085bb40
xachman <zironside@hotmail.com>
2023-11-23 01:50:05 +0000

    use cluster issuer and work on node selectors

--- services.sh
+++ services.sh
@@ -1,10 +1,12 @@
 helm repo add jetstack https://charts.jetstack.io
+helm repo add traefik https://traefik.github.io/charts
 helm repo update
-#helm install traefik traefik/traefik --values kubernetes/treafik-values.yml 
+helm install traefik traefik/traefik --values kubernetes/treafik-values.yml 
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.13.2/cert-manager.crds.yaml
 helm install \
   cert-manager jetstack/cert-manager \
   --namespace cert-manager \
   --create-namespace \
   --version v1.13.2 \
+  --values kubernetes/cert-manager-values.yml
   # --set installCRDs=true
